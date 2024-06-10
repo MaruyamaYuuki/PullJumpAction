127bca2c6fef09ea22b672161f8ba84c6aff2f4f
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-06-10 12:03:50 +0900

    En1_09_サウンド・シーン

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -22,17 +22,19 @@ public class ItemScript : MonoBehaviour
     }
 
     private Animator animator;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //DestroySelf();
         animator.SetTrigger("Get");
+        audioSource.Play();
     }
 
     // Update is called once per frame
