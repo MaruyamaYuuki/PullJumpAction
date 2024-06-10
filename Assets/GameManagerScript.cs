1c014879842535405bea6ae98ca676e9d53a19d7
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-06-10 13:04:54 +0900

    タイトルへ戻る

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManagerScript : MonoBehaviour
 {
     public string nextSceneName;
+    public string titleScene;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,11 @@ public class GameManagerScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(nextSceneName);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(titleScene);
         }
     }
 }
